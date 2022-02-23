728beaf110b83db765c4623ba2bc21a1e8cbe139
Dolinin2021 <programmistdolinin@mail.ru>
2022-02-23 12:38:26 +0600

    fixed phones/views.py

--- task_1/phones/views.py
+++ task_1/phones/views.py
@@ -1,6 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from phones.models import Phone
+
+
+SORT_MAP = {
+    'name': 'name',
+    'min_price': 'price',
+    'max_price': '-price',
+}
 
 
 def index(request):
@@ -8,34 +16,25 @@ def index(request):
 
 
 def show_catalog(request):
-
     template = 'catalog.html'
-    catalog_objects = Phone.objects.all()
-    context = {'phones': catalog_objects}
     sorting = request.GET.get('sort')
-
-    if sorting == 'name':
-        sort_fields = Phone.objects.order_by('name')
+    if sorting:
+        sort_fields = Phone.objects.order_by(SORT_MAP[sorting])
         context = {'phones': sort_fields}
         return render(request, template, context)
-
-    elif sorting == 'min_price':
-        sort_fields = Phone.objects.order_by('price')
-        context = {'phones': sort_fields}
-        return render(request, template, context)
-
-    elif sorting == 'max_price':
-        sort_fields = Phone.objects.order_by('-price')
-        context = {'phones': sort_fields}
-        return render(request, template, context)
-
     else:
+        catalog_objects = Phone.objects.all()
+        context = {'phones': catalog_objects}
         return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phone = Phone.objects.get(slug=slug)
-    print(phone)
-    context = {'phone': phone}
-    return render(request, template, context)
+    try:
+        phone = Phone.objects.get(slug=slug)
+        print(phone)
+        context = {'phone': phone}
+        return render(request, template, context)
+    except Phone.DoesNotExist:
+        return HttpResponse('Телефон не найден. \n'
+                            'Проверьте введенные данные и попробуйте еще раз.')
